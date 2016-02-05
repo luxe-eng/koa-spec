71e3540520c8e4503830778a635ae85b3f099955
Nicolas Gramlich <nicolas@luxevalet.com>
2016-02-05 01:40:18 -0800

    YAML spec loading and $ref dereferencing.
    First stab at koa middleware.
    
    Fixed test from impostering.
    
    Simple yaml parsing.
    
    Added chai subset.
    
    Simple yaml parsing and dereferencing working.
    
    Added test for unknown route.
    
    Using http-status module.
    
    Added test failing on unkown ref.
    
    Shortened error finding chain.
    
    Added test parsing invalid yaml.
    
    Cleanup.
    
    More dereferencing tests.
    
    Cleanup.

--- test/support.js
+++ test/support.js
@@ -1,5 +1,13 @@
 'use strict';
 
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiSubset = require('chai-subset');
 
+chai.config.includeStack = true;
+
+chai.use(chaiAsPromised);
+chai.use(chaiSubset);
+
+global.chaiAsPromised = chaiAsPromised;
 global.expect = chai.expect;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,93 @@
 'use strict';
 
+const koa = require('koa');
+const http = require('http');
+const HTTPStatus = require('http-status');
 const koaspec = require('../');
+const supertest = require('supertest-as-promised');
 
 describe('koaspec', function () {
-  it('returns the answer to life the universe and everything.', function () {
-    expect(koaspec()).to.be.eql(42);
+  describe('parsing', function () {
+    it('throws trying to parse invalid yaml file.', function* () {
+      expect(koaspec.bind(koaspec, 'test/data/invalid.yaml')).to.throw('YAMLException');
+    });
+
+    describe('references', function () {
+      it('throws trying to pass invalid refOptions.', function* () {
+        const options = {
+          refOptions : 'Invalid'
+        };
+
+        expect(koaspec.bind(koaspec, 'test/data/simple.yaml', options)).to.throw('options must be an Object');
+      });
+
+      it('throws trying to parse missing $refs.', function () {
+        expect(koaspec.bind(koaspec, 'test/data/simple_ref_unknown.yaml')).to.throw('#/definitions/Unknown');
+      });
+
+      it('dereferences schema $refs.', function () {
+        const spec = koaspec('test/data/simple_ref.yaml');
+
+        const actual = spec.spec.paths['/'].get.responses['200'].schema;
+        const expected = {
+          title      : 'Index',
+          type       : 'object',
+          properties : {
+            hello : {
+              type    : 'string',
+              example : 'world'
+            }
+          }
+        };
+        expect(actual).to.containSubset(expected);
+      });
+
+      it('does not dereference local references if not asked to.', function* () {
+        const options = {
+          refOptions : {
+            filter : [/* 'local' */]
+          }
+        };
+
+        const spec = koaspec('test/data/simple_ref.yaml', options);
+
+        const actual = spec.spec.paths['/'].get.responses['200'].schema;
+        const expected = {
+          $ref : "#/definitions/Index"
+        };
+        expect(actual).to.containSubset(expected);
+      });
+    });
   });
-});
+
+  describe('routes', function () {
+    it('returns the answer to life the universe and everything.', function* () {
+      const app = koa();
+
+      const spec = koaspec('test/data/simple.yaml');
+
+      app.use(spec.routes());
+
+      // TODO Make this check for the actual endpoint of the simple spec!
+
+      const res = yield supertest(http.createServer(app.callback()))
+        .get('/answer')
+        .expect(HTTPStatus.OK);
+
+      expect(res.body).to.eql(42);
+    });
+
+    it('ignores calling unknown routes.', function* () {
+      const app = koa();
+
+      const spec = koaspec('test/data/simple.yaml');
+
+      app.use(spec.routes());
+
+      yield supertest(http.createServer(app.callback()))
+        .get('/unknown')
+        .expect(HTTPStatus.NOT_FOUND);
+    });
+  });
+})
+;
