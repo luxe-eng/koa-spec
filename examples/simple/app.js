3c2ce2c3af5ffb98503923093dff4b4e2d5fc3b4
Nicolas Gramlich <nicolas@luxevalet.com>
2016-02-19 00:32:07 -0800

    Final codeclimate fixes.

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
+const path = require('path');
 const koa = require('koa');
 const koaspec = require('../..'); // 'koa-spec'
 
 const app = koa();
 
-const OPTIONS = { routerOptions : { controllerDirectory : __dirname + '/controllers' } };
+const OPTIONS = { routerOptions : { controllerDirectory : path.join(__dirname, '/controllers') } };
 
-const spec = koaspec(__dirname + '/data/api.yaml', OPTIONS);
+const spec = koaspec(path.join(__dirname, '/data/api.yaml'), OPTIONS);
 const router = spec.router();
 app.use(router.routes());
 
